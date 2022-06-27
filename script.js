f3ad5d8b1fa2232f608bf53852b8a01612cebd15
diambere <diambere.cisse@outlook.com>
2022-06-27 14:23:38 +0200

    first commit

--- script.js
+++ script.js
@@ -10,9 +10,18 @@ Nous allons apprendre
 
 a=10  // 10 rentre dans la variable A
 b=50
-c="80"
+c="80" 
+b=90 // B contient 90 car dernière valeur entrée
 console.error(a);
 console.error(a+b);
-console.error(typeof(a));
-console.error(a+c);
-console.error(a+parseInt(c));
+console.error(typeof(a));   //number
+console.error(typeof(c));   //string
+console.error(a+c);   // affiche "1080" dans la console (concaténation)
+console.error(a+parseInt(c));   //transforme C en nombre et addition
+
+
+// EXO : LES VARIABLES
+
+SEL = 90;
+SUCRE = 60;
+console.error("Nous avons du sel en Q. de " + SEL + "g. Nous avons du sucre en quantité de " + SUCRE + "g.")
