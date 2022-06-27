2ccb23d9501d39e4e1fa21db2df4aadadfd7e7b4
diambere <diambere.cisse@outlook.com>
2022-06-27 14:02:29 +0200

    first commit

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+
+
+/* Commentaire :
+Nous allons apprendre 
+1. Les variables
+2. Les conditions
+3. Les boucles
+4. Les fonctions */
+
+// Les variables
+
 console.error("test");
 document.write("very pleased to meet you!");
